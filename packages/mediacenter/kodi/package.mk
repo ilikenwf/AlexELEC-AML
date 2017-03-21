28d92c8c0cb12a93f6aaaa573d0f5f0db1a72bfd
Alexandr <alex@alexelec.in.ua>
2017-03-21 07:31:26 +0200

    kodi: bump to v17.1 final

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -4,8 +4,8 @@
 ################################################################################
 
 PKG_NAME="kodi"
-GIT_VERSION="5814568"
-PKG_VERSION="17.1-rc2-$GIT_VERSION"
+GIT_VERSION="fc1619b"
+PKG_VERSION="17.1-$GIT_VERSION"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
