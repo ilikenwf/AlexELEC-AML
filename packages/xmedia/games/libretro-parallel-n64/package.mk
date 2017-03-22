97f8523bffb067d0685dbf733966a494d3c226e9
Alexandr <alex@alexelec.in.ua>
2017-03-22 08:17:02 +0200

    libretro-parallel-n64: up a9101ca

--- packages/xmedia/games/libretro-parallel-n64/package.mk
+++ packages/xmedia/games/libretro-parallel-n64/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="libretro-parallel-n64"
-PKG_VERSION="e2b1609"
+PKG_VERSION="a9101ca"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPLv2"
