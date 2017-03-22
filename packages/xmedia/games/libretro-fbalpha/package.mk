f76e58dee66a24bca3c1fdfbdaac89ceff0f3c48
Alexandr <alex@alexelec.in.ua>
2017-03-22 07:41:46 +0200

    libretro-fbalpha: up 84041bc

--- packages/xmedia/games/libretro-fbalpha/package.mk
+++ packages/xmedia/games/libretro-fbalpha/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="libretro-fbalpha"
-PKG_VERSION="835951a"
+PKG_VERSION="84041bc"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Non-commercial"
