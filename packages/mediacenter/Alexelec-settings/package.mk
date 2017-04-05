90d7997bcbe2425d9686b4c6635ed35d0357ed8b
Alexandr <alex@alexelec.in.ua>
2017-04-05 12:08:19 +0300

    acestream: Remake TTV playlist

--- packages/mediacenter/Alexelec-settings/package.mk
+++ packages/mediacenter/Alexelec-settings/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="Alexelec-settings"
-PKG_VERSION="f965bae"
+PKG_VERSION="b417894"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
