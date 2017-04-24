d221450fe9f705b500f964fa7d500b81c9e2aec9
Alexandr <alex@alexelec.in.ua>
2017-04-24 21:55:49 +0300

    Alexelec-settings: add Install to NAND

--- packages/mediacenter/Alexelec-settings/package.mk
+++ packages/mediacenter/Alexelec-settings/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="Alexelec-settings"
-PKG_VERSION="985a1e0"
+PKG_VERSION="b474d07"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
