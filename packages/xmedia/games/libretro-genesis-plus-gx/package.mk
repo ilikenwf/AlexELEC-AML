36bffb0cdb1bf2dca0f620a9bc59fdeaae8f5942
Alexandr <alex@alexelec.in.ua>
2017-05-01 09:34:51 +0300

    games: upd libretro-genesis-plus-gx

--- packages/xmedia/games/libretro-genesis-plus-gx/package.mk
+++ packages/xmedia/games/libretro-genesis-plus-gx/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="libretro-genesis-plus-gx"
-PKG_VERSION="f64e755"
+PKG_VERSION="365a28c"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="Non-commercial"
