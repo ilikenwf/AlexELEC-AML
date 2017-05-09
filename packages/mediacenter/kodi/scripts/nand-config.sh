ac1725d8f5ee0d9b47175be4e932c2bb7f111d2c
Alexandr <alex@alexelec.in.ua>
2017-05-09 18:24:09 +0300

    kodi: Hide [Switch to Android] if the system is installed in nand

--- packages/mediacenter/kodi/scripts/nand-config.sh
+++ packages/mediacenter/kodi/scripts/nand-config.sh
@@ -10,6 +10,12 @@ cat /proc/cmdline | grep 'BOOT_IMAGE=kernel.img' > /dev/null
 if [ $? == 0 ]; then
   mkdir -p /storage/nand
   touch /var/lock/nand.lock
+else
+  if [ ! -f /storage/.config/kodi.skins/nand.install ]; then
+    touch /storage/.config/kodi.skins/nand.install
+    cp -f /usr/share/kodi/config/Nox-DialogButtonMenu.xml /storage/.config/kodi.skins/skin.aeon.nox.5ae/1080i/DialogButtonMenu.xml
+    cp -f /usr/share/kodi/config/Estuary-DialogButtonMenu.xml /storage/.config/kodi.skins/skin.estuary/xml/DialogButtonMenu.xml
+  fi
 fi
 
 exit 0
