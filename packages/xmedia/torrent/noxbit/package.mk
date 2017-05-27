f03d1174b661caac7250118400e00abcdbb393ae
Alexandr <alex@alexelec.in.ua>
2017-05-27 10:25:00 +0300

    noxbit: bump to version 26.05.17

--- packages/xmedia/torrent/noxbit/package.mk
+++ packages/xmedia/torrent/noxbit/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="noxbit"
-PKG_VERSION="770b9ed"
+PKG_VERSION="12f0c11"
 PKG_REV="2"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
