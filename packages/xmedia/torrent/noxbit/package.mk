b02d4545b8f6786928becc46be3ae159791ec995
Alexandr <alex@alexelec.in.ua>
2017-06-04 16:10:43 +0300

    noxbit: bump to 02.06.2017

--- packages/xmedia/torrent/noxbit/package.mk
+++ packages/xmedia/torrent/noxbit/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="noxbit"
-PKG_VERSION="05b1a6c"
+PKG_VERSION="f8e4b90"
 PKG_REV="2"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
