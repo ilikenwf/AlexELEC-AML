5f5d80984f2b73423affdfcbb0a217d68d027e5e
Alexandr <alex@alexelec.in.ua>
2017-06-04 14:13:33 +0300

    squashed gperf 3.1 fixes & upd systemd-232

--- packages/devel/libcap/package.mk
+++ packages/devel/libcap/package.mk
@@ -1,20 +1,6 @@
 ################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-#      Copyright (C) 2010-2011 Roman Weber (roman@openelec.tv)
-#
-#  OpenELEC is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  OpenELEC is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+#      This file is part of Alex@ELEC - http://www.alexelec.in.ua
+#      Copyright (C) 2011-2017 Alexandr Zuyev (alex@alexelec.in.ua)
 ################################################################################
 
 PKG_NAME="libcap"
@@ -29,11 +15,10 @@ PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="devel"
 PKG_SHORTDESC="libcap: A library for getting and setting POSIX.1e capabilities"
 PKG_LONGDESC="As of Linux 2.2.0, the power of the superuser has been partitioned into a set of discrete capabilities (in other places, these capabilities are know as privileges). The contents of the libcap package are a library and a number of simple programs that are intended to show how an application/daemon can be protected (with wrappers) or rewritten to take advantage of this fine grained approach to constraining the danger to your system from programs running as 'root'."
-
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="no"
 
-post_unpack() {
+post_patch() {
   mkdir -p $ROOT/$PKG_BUILD/.$HOST_NAME
   cp -r $ROOT/$PKG_BUILD/* $ROOT/$PKG_BUILD/.$HOST_NAME
   mkdir -p $ROOT/$PKG_BUILD/.$TARGET_NAME 
--- packages/sysutils/systemd/package.mk
+++ packages/sysutils/systemd/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="systemd"
-PKG_VERSION="230"
+PKG_VERSION="232"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
