6de05c34e7963634a63aa8493d33f3dcb95b69f6
alexelec <alexelecv5@gmail.com>
2017-06-17 11:29:59 +0300

    add pkg oscam

--- packages/oem/package.mk
+++ packages/oem/package.mk
@@ -41,7 +41,8 @@ post_install() {
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET plexmediaserver"
 
 # tv services
-  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET vdr-all wicard tvheadend"
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET vdr-all tvheadend"
+  PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET wicard oscam"
 
 # tools
   PKG_DEPENDS_TARGET="$PKG_DEPENDS_TARGET ImageMagick mc aml-vnc scan-s2 scan-m3u serviceref"
