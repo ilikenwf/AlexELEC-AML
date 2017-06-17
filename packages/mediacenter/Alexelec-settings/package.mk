bc0256c5f8687a8a54b1a4a0b1169fb2f35d2b07
alexelec <alexelecv5@gmail.com>
2017-06-17 14:00:45 +0300

    Alexelec-settings: add Oscam

--- packages/mediacenter/Alexelec-settings/package.mk
+++ packages/mediacenter/Alexelec-settings/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="Alexelec-settings"
-PKG_VERSION="91b0c4b"
+PKG_VERSION="6006cea"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
