d0f097e4006a7b32e4c0ea67416313a6f8d41b78
alexelec <alexelecv5@gmail.com>
2017-06-19 16:26:47 +0300

    Alexelec-settings: fix create logos

--- packages/mediacenter/Alexelec-settings/package.mk
+++ packages/mediacenter/Alexelec-settings/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="Alexelec-settings"
-PKG_VERSION="6006cea"
+PKG_VERSION="03c6010"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
