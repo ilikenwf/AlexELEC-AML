0af4bd193a142c82d34e4f360b1cf61753209a73
alexelec <alexelecv5@gmail.com>
2017-06-20 15:31:01 +0300

    upd Alexelec-settings

--- packages/mediacenter/Alexelec-settings/package.mk
+++ packages/mediacenter/Alexelec-settings/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="Alexelec-settings"
-PKG_VERSION="03c6010"
+PKG_VERSION="99c14cb"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
