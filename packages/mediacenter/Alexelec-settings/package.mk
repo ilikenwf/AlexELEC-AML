727e1e415d40fdcd44b276801e5624a75c75bd68
alexelec <alexelecv5@gmail.com>
2017-06-27 12:54:16 +0300

    Alexelec-settings: add display resolution setup

--- packages/mediacenter/Alexelec-settings/package.mk
+++ packages/mediacenter/Alexelec-settings/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="Alexelec-settings"
-PKG_VERSION="99c14cb"
+PKG_VERSION="1b17c3b"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="prop."
