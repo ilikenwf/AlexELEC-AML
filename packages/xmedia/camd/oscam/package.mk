4663a6f967a54316e944e3eb8022630bdd320a3e
alexelec <alexelecv5@gmail.com>
2017-07-01 12:01:40 +0300

    oscam: bump to 11390

--- packages/xmedia/camd/oscam/package.mk
+++ packages/xmedia/camd/oscam/package.mk
@@ -4,8 +4,8 @@
 ################################################################################
 
 PKG_NAME="oscam"
-PKG_VERSION="d668e52"
-PKG_VERSION_NUMBER="11388"
+PKG_VERSION="b29f5c6"
+PKG_VERSION_NUMBER="11390"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="GPL"
