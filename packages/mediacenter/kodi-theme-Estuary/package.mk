844a98030aca26cd4670b93de6360e0ec0c928bf
alexelec <alexelecv5@gmail.com>
2017-07-05 16:52:54 +0300

    kodi: bump to 17.4-rc1-6034422

--- packages/mediacenter/kodi-theme-Estuary/package.mk
+++ packages/mediacenter/kodi-theme-Estuary/package.mk
@@ -4,7 +4,7 @@
 ################################################################################
 
 PKG_NAME="kodi-theme-Estuary"
-GIT_VERSION="6abeebd"
+GIT_VERSION="6034422"
 PKG_VERSION="17.4-rc1-$GIT_VERSION"
 PKG_REV="1"
 PKG_ARCH="any"
