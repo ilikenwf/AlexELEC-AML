a5001ba12d02ee1f29d7520ad8d4bc6ff37f24ac
alexelec <alexelecv5@gmail.com>
2017-07-09 18:45:57 +0300

    libamcodec: update from buildroot 2017-06-30 (afl1)

--- packages/multimedia/libamcodec/package.mk
+++ packages/multimedia/libamcodec/package.mk
@@ -14,8 +14,8 @@ case $TARGET_KERNEL_ARCH in
     PKG_URL="https://github.com/codesnake/libamcodec/archive/$PKG_VERSION.tar.gz"
     ;;
   arm64)
-    PKG_VERSION="2fba80c"
-    PKG_URL="https://github.com/surkovalex/libamcodec/archive/$PKG_VERSION.tar.gz"
+    PKG_VERSION="bccd426"
+    PKG_URL="https://github.com/afl1/libamcodec/archive/$PKG_VERSION.tar.gz"
     ;;
 esac
 PKG_DEPENDS_TARGET="toolchain alsa-lib"
