be470ee57eef5ef49f3fd7ec7da62b9de9a93265
alexelec <alexelecv5@gmail.com>
2017-07-13 13:47:40 +0300

    tvheadend: fix suspend mode for Availink avl6862/6882

--- packages/xmedia/tvservice/tvheadend/sleep.d.serial/02-tvh-sleep.sh
+++ packages/xmedia/tvservice/tvheadend/sleep.d.serial/02-tvh-sleep.sh
@@ -6,15 +6,18 @@
 
 SERVICE="tvheadend.service"
 SERVICE_DIR="/storage/.cache/services"
+AVL_MODULE_RUN="/usr/bin/avl-dvb.run"
 
 case $1 in
   pre)
     if [ -f "$SERVICE_DIR/tvheadend.conf" ] ; then
       systemctl stop "$SERVICE"
+      [ -x "$AVL_MODULE_RUN" ] && $AVL_MODULE_RUN del
     fi
     ;;
   post)
     if [ -f "$SERVICE_DIR/tvheadend.conf" ] ; then
+      [ -x "$AVL_MODULE_RUN" ] && $AVL_MODULE_RUN
       systemctl start "$SERVICE"
     fi
     ;;
