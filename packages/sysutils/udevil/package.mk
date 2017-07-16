b1a7caaa969f5fd27e87c408f65e01523d328fbe
alexelec <alexelecv5@gmail.com>
2017-07-16 10:15:33 +0300

    udevil: mount before services that can use external storage

--- packages/sysutils/udevil/package.mk
+++ packages/sysutils/udevil/package.mk
@@ -1,19 +1,6 @@
 ################################################################################
-#      This file is part of OpenELEC - http://www.openelec.tv
-#      Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
-#
-#  OpenELEC is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  OpenELEC is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+#      This file is part of Alex@ELEC - http://www.alexelec.in.ua
+#      Copyright (C) 2011-2017 Alexandr Zuyev (alex@alexelec.in.ua)
 ################################################################################
 
 PKG_NAME="udevil"
@@ -27,7 +14,6 @@ PKG_DEPENDS_TARGET="toolchain systemd glib"
 PKG_SECTION="system"
 PKG_SHORTDESC="udevil: Mounts and unmounts removable devices and networks without a password."
 PKG_LONGDESC="udevil Mounts and unmounts removable devices and networks without a password (set suid), shows device info, monitors device changes. Emulates mount's and udisks's command line usage and udisks v1's output. Includes the devmon automounting daemon."
-
 PKG_IS_ADDON="no"
 PKG_AUTORECONF="yes"
 
